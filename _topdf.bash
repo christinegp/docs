c7e4143a66388a52bb8c28f4ae3cb3c7ac715d74
Manoj Das <manoj_das@yahoo.com>
2015-09-23 15:01:37 -0700

    automated the topdf script

--- _topdf.bash
+++ _topdf.bash
@@ -1,9 +1,34 @@
 #!/bin/bash          
 H='http://localhost:4000/docs'
-HC=$H/concepts
-HCS="$HC/ $HC/communications/ $HC/content/"
-echo $H
-echo $HC
-echo $HCS
 
-wkhtmltopdf $H/ $HCS $1
+function massagefile {
+	fname=$1
+	fname=${fname/_site/$H}
+	fname=${fname/%index.html/}
+	#fname=${fname/%.html/}
+	echo $fname
+}
+FILES=
+function findhtml {
+	for file in $1/*.html
+	do
+	    if [[ -f $file ]]; then
+	        #echo File $file
+	        fname=`massagefile $file`
+	        FILES="$FILES $fname"
+	    fi
+	done
+	for dir in $1/*
+	do
+	    if [[ -d $dir ]]; then
+	    	findhtml $dir
+	    fi
+	done
+
+}
+
+findhtml "_site"
+#echo "----------------"
+echo $FILES
+
+wkhtmltopdf --footer-center "Stellar Loyalty Confidential" $FILES $1
